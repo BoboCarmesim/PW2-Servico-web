1a056c9fc6b61fc394402fbf137cbe7d999c857e
Rodrigo Borges <digoborges999@gmail.com>
2025-10-08 20:17:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
+import{filmes} from "../data/lista filmes/filmes.js"
+
 function Home (){
+    console.log(filmes)
+
+    const primeirofilme= filmes[0]
+
+    const estiloImagem ={
+        width: "300px"
+    }
+
     return <div>
         <h1>Filmes</h1>
 
-        <p>Listagem de filmes aqui:</p>
+        <ul>
+            <li>
+                <p>Nome do filme:</p>
+
+                <p>Título: {primeirofilme.titulo} </p>
+                <p>Ano: {primeirofilme.ano}</p>
+                <p>Gênero: {primeirofilme.genero}</p>
+                <p>Nota: {primeirofilme.nota}</p>
+
+                <img style={estiloImagem} src={primeirofilme.poster}/>
+            </li>
+        </ul>
     </div>
 }
 
